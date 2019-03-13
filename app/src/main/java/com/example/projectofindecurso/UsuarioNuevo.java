f3e209354911950903f864d5a36918a92c750c66
guillermopelaez <gapelaezs98@elpuig.xeill.net>
2019-03-13 17:21:13 +0100

    usuario nuevo

---
+++ app/src/main/java/com/example/projectofindecurso/UsuarioNuevo.java
@@ -0,0 +1,13 @@
+package com.example.projectofindecurso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UsuarioNuevo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario_nuevo);
+    }
+}
