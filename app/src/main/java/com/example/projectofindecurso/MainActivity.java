d6f2874c39a8a2b011bfd524b47435d517b35277
guillermopelaez <gapelaezs98@elpuig.xeill.net>
2019-03-19 17:07:28 +0100

    usuario

--- app/src/main/java/com/example/projectofindecurso/MainActivity.java
+++ app/src/main/java/com/example/projectofindecurso/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.projectofindecurso;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        findViewById(R.id.createAcount).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UsuarioNuevo.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/projectofindecurso/UsuarioNuevo.java
+++ app/src/main/java/com/example/projectofindecurso/UsuarioNuevo.java
@@ -1,7 +1,9 @@
 package com.example.projectofindecurso;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class UsuarioNuevo extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class UsuarioNuevo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuario_nuevo);
+        findViewById(R.id.botton_correoElectronico).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UsuarioNuevo.this, RegistroUsuarioNuevo.class);
+                startActivity(intent);
+            }
+        });
     }
 }
