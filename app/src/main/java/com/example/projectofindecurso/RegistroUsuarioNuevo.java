afc62623092488bea60ee8d2269812d3257c674c
guillermopelaez <gapelaezs98@elpuig.xeill.net>
2019-03-19 15:52:57 +0100

    usuario nuevo2

---
+++ app/src/main/java/com/example/projectofindecurso/RegistroUsuarioNuevo.java
@@ -0,0 +1,13 @@
+package com.example.projectofindecurso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistroUsuarioNuevo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_usuario_nuevo);
+    }
+}
