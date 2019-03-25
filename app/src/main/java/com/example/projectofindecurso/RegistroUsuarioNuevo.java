11e253914ff5e6c5cfe459e812a902938adf6871
guillermopelaez <gapelaezs98@elpuig.xeill.net>
2019-03-25 20:07:16 +0100

    plantilla usuario nuevo

--- app/src/main/java/com/example/projectofindecurso/RegistroUsuarioNuevo.java
+++ app/src/main/java/com/example/projectofindecurso/RegistroUsuarioNuevo.java
@@ -2,12 +2,24 @@ package com.example.projectofindecurso;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class RegistroUsuarioNuevo extends AppCompatActivity {
+
+    private CheckBox seleccionDireccion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario_nuevo);
+        seleccionDireccion = (CheckBox) findViewById(R.id.seleccion_direccion_envio);
+    }
+    public void loguearCheckbox(View v) {
+        if (!seleccionDireccion.isChecked()) {
+            String s = "ERROR: " + "campo obligatorio";
+            Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+        }
     }
 }
