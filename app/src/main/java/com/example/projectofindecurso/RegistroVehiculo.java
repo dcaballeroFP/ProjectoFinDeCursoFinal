1e6a4dcb3c7673612716bd75c94e5468ad85f837
guillermopelaez <gapelaezs98@elpuig.xeill.net>
2019-03-26 16:48:54 +0100

    plantilla usuario nuevo2

---
+++ app/src/main/java/com/example/projectofindecurso/RegistroVehiculo.java
@@ -0,0 +1,13 @@
+package com.example.projectofindecurso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistroVehiculo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_vehiculo);
+    }
+}
