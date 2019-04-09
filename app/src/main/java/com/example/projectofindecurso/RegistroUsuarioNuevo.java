62ac9b2621031b9616637e358720699e480c0a01
Christian <cruizm97@elpuig.xeill.net>
2019-04-09 15:35:12 +0200

    Medio terminado registro con google

--- app/src/main/java/com/example/projectofindecurso/RegistroUsuarioNuevo.java
+++ app/src/main/java/com/example/projectofindecurso/RegistroUsuarioNuevo.java
@@ -2,11 +2,24 @@ package com.example.projectofindecurso;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 
 public class RegistroUsuarioNuevo extends AppCompatActivity {
+
+    private EditText nombreview;
+    private EditText apellidoview;
+    private EditText emailview;
+    int contador;
 
     private CheckBox seleccionDireccion;
 
@@ -15,6 +28,36 @@ public class RegistroUsuarioNuevo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario_nuevo);
         seleccionDireccion = (CheckBox) findViewById(R.id.seleccion_direccion_envio);
+
+        nombreview=findViewById(R.id.NombreRegistro);
+        apellidoview=findViewById(R.id.ApellidoRegistro);
+        emailview=findViewById(R.id.EmailRegistro);
+
+
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null) {
+            nombreview.setText(firebaseUser.getDisplayName());
+            apellidoview.setText(firebaseUser.getDisplayName());
+            emailview.setText(firebaseUser.getEmail());
+            Log.e("Error", nombreview.getText().toString());
+
+            for (int i = 0; i <nombreview.length() ; i++) {
+                char c = nombreview.getText().toString().charAt(i);
+                if(c== ' '){
+                    nombreview.setText(nombreview.getText().toString().substring(0,contador));
+                    apellidoview.setText(nombreview.getText().toString().substring(contador));
+
+                    break;
+                }else {
+                    contador++;
+                }
+
+            }
+
+        }
+
+
     }
     public void loguearCheckbox(View v) {
         if (!seleccionDireccion.isChecked()) {
@@ -22,4 +65,6 @@ public class RegistroUsuarioNuevo extends AppCompatActivity {
             Toast.makeText(this, s, Toast.LENGTH_LONG).show();
         }
     }
+
+
 }
--- app/src/main/java/com/example/projectofindecurso/UsuarioNuevo.java
+++ app/src/main/java/com/example/projectofindecurso/UsuarioNuevo.java
@@ -4,19 +4,57 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class UsuarioNuevo extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuario_nuevo);
-        findViewById(R.id.botton_correoElectronico).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(UsuarioNuevo.this, RegistroUsuarioNuevo.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                signIn();
             }
         });
+
+        comeIn();
+    }
+
+    void comeIn(){
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser != null) {
+            startActivity(new Intent(this, RegistroUsuarioNuevo.class));
+            finish();
+        }
+    }
+
+    void signIn(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.GoogleBuilder().build()))
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                comeIn();
+
+            }
+        }
     }
 }
