3ffc65aee96ca8283e7d73d533278e9a135d589f
guillermopelaez <gapelaezs98@elpuig.xeill.net>
2019-05-30 19:51:51 +0200

    prueba

--- app/src/main/java/com/example/projectofindecurso/Ayuda/ActivityAyuda.java
+++ app/src/main/java/com/example/projectofindecurso/Ayuda/ActivityAyuda.java
@@ -4,41 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.projectofindecurso.R;
 
 public class ActivityAyuda extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ayuda);
+        getSupportActionBar().hide();
+
     }
 
-    public void ejecutar_ayuda_aeropuerto(View v){
-        Intent i = new Intent(this, ActivityAyudaAeropuerto.class);
-        startActivity(i);
-    }
-
-    public void ejecutar_ayuda_aparcamiento(View v){
-        Intent i = new Intent(this, ActivityAyudaAparcamiento.class);
-        startActivity(i);
-    }
-
-    public void ejecutar_ayuda_localizacion(View v){
-        Intent i = new Intent(this, ActivityAyudaLocalizacion.class);
-        startActivity(i);
-    }
-
-    public void ejecutar_ayuda_registro(View v){
-        Intent i = new Intent(this, ActivityAyudaRegistro.class);
-        startActivity(i);
-    }
-
-    public void ejecutar_ayuda_restaurante(View v){
-        Intent i = new Intent(this, ActivityAyudaRestaurante.class);
-        startActivity(i);
-    }
 
 
 }
